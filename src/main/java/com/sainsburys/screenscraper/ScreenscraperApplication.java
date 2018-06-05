d83e41643641ae4cfac59998e7a738b61fc39dea
Dominic Keefe <dom.keefe@gmail.com>
2018-06-05 15:51:46 +0100

    Initial commit of main class

---
+++ src/main/java/com/sainsburys/screenscraper/ScreenscraperApplication.java
@@ -0,0 +1,12 @@
+package com.sainsburys.screenscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenscraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenscraperApplication.class, args);
+	}
+}
